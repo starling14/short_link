44f59725f2727ab6bcd8af16267f917c885371bb
starling14 <volodymyr.shpak@gmail.com>
2017-01-30 12:27:53 +0200

    refactoring of the redirection middleware;

--- app/middleware/user_url_redirect.rb
+++ app/middleware/user_url_redirect.rb
@@ -7,15 +7,29 @@ class UserUrlRedirect
     uri = Rack::Request.new(env).path.gsub('/', '')
     if short_uri?(uri)
       pk_id = ShortUriGenerator.decode(uri)
-      offset = ShortUriGenerator.id_offset(ShortUri::SHORT_URI_MIN_LENGTH)
-      s_uri = ShortUri.find(pk_id - offset)
-      [301, {'Location' => "#{s_uri.short_uri_protocol.name}://#{s_uri.user_url}"}, []]
+      s_uri = ShortUri.find(pk_id_without_offset(pk_id))
+      generate_response("#{s_uri.short_uri_protocol.name}://#{s_uri.user_url}")
     else
       @app.call(env)
     end
   end
 
+  # check if uri path is short uri
+  # because the short uri does not contain vowels we can detect it
+  private
   def short_uri?(path)
     !(path =~ /[euioaEUIOA]/) && path.length >= ShortUri::SHORT_URI_MIN_LENGTH
+  end
+
+  def pk_offset
+    @pk_offset ||= ShortUriGenerator.id_offset(ShortUri::SHORT_URI_MIN_LENGTH)
+  end
+
+  def pk_id_without_offset(pk_id)
+    pk_id - pk_offset
+  end
+
+  def generate_response(location)
+    [301, {'Location' => location}, []]
   end
 end
